1a0356e15384cd5e0537cc1954ec6c5ae346f8cb
James <oneinfinity@yahoo.com>
2019-10-25 10:29:55 -0500

    add test ci

---
+++ HelloMvc/HelloMvc/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMvc.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer {Id = 1, Name= "Nick Endersk" },
+                new Customer { Id = 1, Name = "Frank Krank" }
+            };
+
+
+            return View();
+        }
+    }
+}
---
+++ TTMCI/TopTenMovies.DataAccess/AllProductsDB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using TopTenMovies.DataAccess.Entities;
+
+namespace TopTenMovies.DataAccess
+{
+    public class AllProductsDB
+    {
+        public List<Product> GetAllProducts()
+        {
+            //string connectionString = SecretConfiguration.ConnectionString;
+
+            //DbContextOptions<TopTenMoviesContext> options = new DbContextOptionsBuilder<TopTenMoviesContext>()
+            //    .UseSqlServer(connectionString)
+            //    .Options;
+
+            //using var context = new TopTenMoviesContext(options);
+
+            List<Product> products = new List<Product>();
+
+            //foreach (Product product in context.Product)
+            //{
+            //    Product displayProduct = new Product();
+
+            //    displayProduct.ProductId = product.ProductId;
+            //    displayProduct.Title = product.Title;
+
+            //    products.Add(displayProduct);
+
+            //}
+
+            return products;
+        }
+    }
+}
